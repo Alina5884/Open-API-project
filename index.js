4a4775853c9ecc3e074d0315317b3191df106566
Alina Dalantaeva <elina.d32@yandex.ru>
2024-09-12 20:59:14 -0400

    Open API project created

---
+++ index.js
@@ -0,0 +1,13 @@
+fetch('https://api.sampleapis.com/coffee/ice')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+    }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem: ', error);
+    })
